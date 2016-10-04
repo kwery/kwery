33af78ad77dd8558cb1223fd97ded34b277f3b7c
abhirama <abhyrama@gmail.com>
2016-10-04 09:17:10 +0530

    SqlQueryExecution filter.

--- src/main/java/dao/SqlQueryExecutionDao.java
+++ src/main/java/dao/SqlQueryExecutionDao.java
@@ -1,13 +1,21 @@
 package dao;
 
+import com.google.common.base.Strings;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.persist.Transactional;
 import models.SqlQueryExecution;
 import models.SqlQueryExecution.Status;
 import ninja.jpa.UnitOfWork;
+import services.scheduler.SqlQueryExecutionSearchFilter;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.LinkedList;
 import java.util.List;
 
 public class SqlQueryExecutionDao {
@@ -59,5 +67,52 @@ public class SqlQueryExecutionDao {
     public List<SqlQueryExecution> getByStatus(Status status) {
         EntityManager m = entityManagerProvider.get();
         return m.createQuery("SELECT e FROM SqlQueryExecution e WHERE e.status = :status").setParameter("status", status).getResultList();
+    }
+
+    @UnitOfWork
+    public List<SqlQueryExecution> filter(SqlQueryExecutionSearchFilter filter) {
+        EntityManager m = entityManagerProvider.get();
+        CriteriaBuilder c = m.getCriteriaBuilder();
+
+        CriteriaQuery<SqlQueryExecution>  q = c.createQuery(SqlQueryExecution.class);
+        Root<SqlQueryExecution> root = q.from(SqlQueryExecution.class);
+
+        List<Predicate> predicates = new LinkedList<>();
+
+        if (filter.getSqlQueryId() != 0) {
+            predicates.add(c.equal(root.get("sqlQuery").get("id"), filter.getSqlQueryId()));
+        }
+
+        if (filter.getExecutionStartStart() != 0) {
+            predicates.add(c.greaterThan(root.get("executionStart"), filter.getExecutionStartStart()));
+        }
+
+        if (filter.getExecutionStartEnd() != 0) {
+            predicates.add(c.lessThan(root.get("executionStart"), filter.getExecutionStartEnd()));
+        }
+
+        if (filter.getExecutionEndStart() != 0) {
+            predicates.add(c.greaterThan(root.get("executionEnd"), filter.getExecutionEndStart()));
+        }
+
+        if (filter.getExecutionEndEnd() != 0) {
+            predicates.add(c.lessThan(root.get("executionEnd"), filter.getExecutionEndEnd()));
+        }
+
+        if (filter.getStatuses() != null && !filter.getStatuses().isEmpty()) {
+            predicates.add(root.get("status").in(filter.getStatuses()));
+        }
+
+        if (!"".equals(Strings.nullToEmpty(filter.getExecutionId()))) {
+            predicates.add(c.equal(root.get("executionId"), filter.getExecutionId()));
+        }
+
+        q.where(predicates.toArray(new Predicate[]{}));
+
+        TypedQuery<SqlQueryExecution> tq = m.createQuery(q)
+                .setMaxResults(filter.getResultCount())
+                .setFirstResult(filter.getPageNumber() * filter.getResultCount());
+
+        return tq.getResultList();
     }
 }
