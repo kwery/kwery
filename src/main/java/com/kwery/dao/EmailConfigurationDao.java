44ce8d1d276454993efe0f3993f13a920c7fa15a
abhirama <abhyrama@gmail.com>
2016-11-22 21:40:40 +0530

    EmailConfigurationDao get api.

--- src/main/java/com/kwery/dao/EmailConfigurationDao.java
+++ src/main/java/com/kwery/dao/EmailConfigurationDao.java
@@ -4,8 +4,13 @@ import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.persist.Transactional;
 import com.kwery.models.EmailConfiguration;
+import ninja.jpa.UnitOfWork;
 
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 public class EmailConfigurationDao {
     protected final Provider<EntityManager> entityManagerProvider;
@@ -26,5 +31,20 @@ public class EmailConfigurationDao {
         }
 
         e.flush();
+    }
+
+    @UnitOfWork
+    public List<EmailConfiguration> get() {
+        EntityManager e = entityManagerProvider.get();
+        CriteriaBuilder cb = e.getCriteriaBuilder();
+        CriteriaQuery<EmailConfiguration> cq = cb.createQuery(EmailConfiguration.class);
+        Root<EmailConfiguration> root = cq.from(EmailConfiguration.class);
+        CriteriaQuery<EmailConfiguration> all = cq.select(root);
+        return e.createQuery(all).getResultList();
+    }
+
+    @UnitOfWork
+    public EmailConfiguration get(int id) {
+        return entityManagerProvider.get().find(EmailConfiguration.class, id);
     }
 }
