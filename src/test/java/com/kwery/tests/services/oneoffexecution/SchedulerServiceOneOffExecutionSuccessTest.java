ee851208db765325683ba6ffc2972ade5abf945f
abhirama <abhyrama@gmail.com>
2016-11-24 14:54:34 +0530

    Sending email on execution success.

--- src/test/java/com/kwery/tests/services/oneoffexecution/SchedulerServiceOneOffExecutionSuccessTest.java
+++ src/test/java/com/kwery/tests/services/oneoffexecution/SchedulerServiceOneOffExecutionSuccessTest.java
@@ -3,7 +3,15 @@ package com.kwery.tests.services.oneoffexecution;
 import com.kwery.models.SqlQuery;
 import com.kwery.models.SqlQueryExecution;
 import com.kwery.services.scheduler.SqlQueryExecutionSearchFilter;
+import com.kwery.tests.fluentlenium.utils.DbUtil;
+import com.ninja_squad.dbsetup.DbSetup;
+import com.ninja_squad.dbsetup.Operations;
+import com.ninja_squad.dbsetup.destination.DataSourceDestination;
+import ninja.postoffice.Mail;
+import ninja.postoffice.Postoffice;
+import ninja.postoffice.mock.PostofficeMockImpl;
 import org.dbunit.DatabaseUnitException;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -12,6 +20,7 @@ import java.util.List;
 
 import static com.google.common.base.Strings.nullToEmpty;
 import static com.kwery.models.SqlQueryExecution.Status.SUCCESS;
+import static com.ninja_squad.dbsetup.Operations.insertInto;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
@@ -24,6 +33,19 @@ import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 
 public class SchedulerServiceOneOffExecutionSuccessTest extends SchedulerServiceOneOffExecutionBaseTest {
+    @Before
+    public void setUpSchedulerServiceOneOffExecutionSuccessTest () {
+        new DbSetup(
+                new DataSourceDestination(DbUtil.getDatasource()),
+                Operations.sequenceOf(
+                        insertInto(SqlQuery.TABLE_QUERY_RUN_EMAIL_RECIPIENT)
+                                .columns(SqlQuery.COLUMN_QUERY_RUN_ID_FK, SqlQuery.COLUMN_EMAIL)
+                                .values(successQueryId, "foo@getkwery.com")
+                                .build()
+                )
+        ).launch();
+    }
+
     @Test
     public void test() throws InterruptedException, DatabaseUnitException, SQLException, IOException {
         long start = System.currentTimeMillis();
@@ -50,5 +72,9 @@ public class SchedulerServiceOneOffExecutionSuccessTest extends SchedulerService
         assertThat(sqlQueryTaskSchedulerHolder.all(), emptyIterable());
         assertThat(oneOffSqlQueryTaskSchedulerReaper.getSqlQueryTaskSchedulerExecutorPairs(), iterableWithSize(1));
         assertThat(schedulerService.ongoingQueryTasks(successQueryId), emptyIterable());
+
+        Mail mail = ((PostofficeMockImpl)getInstance(Postoffice.class)).getLastSentMail();
+
+        assertThat(mail, notNullValue());
     }
 }
