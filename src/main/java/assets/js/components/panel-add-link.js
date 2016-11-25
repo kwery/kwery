bddb4b3958685f2de26c5e3bbc509568029f5b2c
abhirama <abhyrama@gmail.com>
2016-11-25 18:54:08 +0530

    Nuked tooltip js and replaced action buttons in list sql query to glyphicons.

--- src/main/java/assets/js/components/panel-add-link.js
+++ src/main/java/assets/js/components/panel-add-link.js
@@ -5,10 +5,6 @@ define(["knockout", "jquery", "text!components/panel-add-link.html"], function (
         self.url = ko.observable(params.url);
         self.tooltipMessage = ko.observable(ko.i18n(params.tooltipMessageKey));
 
-        $('[data-toggle="tooltip"]').tooltip({
-            title: ko.i18n(params.tooltipMessageKey)
-        });
-
         return self;
     }
     return { viewModel: viewModel, template: template };
