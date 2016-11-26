7d41864ed72a5b219216f8b3a2f85e7e6cd1511e
abhirama <abhyrama@gmail.com>
2016-11-26 18:26:23 +0530

    Incorporated NinjaServerRule and MySqlDockerRule in UI onboarding test classes.

--- src/test/java/com/kwery/tests/fluentlenium/onboarding/OnboardingShowHomeScreenUiTest.java
+++ src/test/java/com/kwery/tests/fluentlenium/onboarding/OnboardingShowHomeScreenUiTest.java
@@ -3,14 +3,16 @@ package com.kwery.tests.fluentlenium.onboarding;
 import com.kwery.models.Datasource;
 import com.kwery.models.SqlQuery;
 import com.kwery.models.SqlQueryExecution;
-import com.kwery.tests.fluentlenium.RepoDashFluentLeniumTest;
-import com.kwery.tests.fluentlenium.user.login.UserLoginPage;
-import com.kwery.tests.fluentlenium.utils.UserTableUtil;
+import com.kwery.tests.util.ChromeFluentTest;
+import com.kwery.tests.util.LoginRule;
+import com.kwery.tests.util.NinjaServerRule;
 import com.ninja_squad.dbsetup.DbSetup;
 import com.ninja_squad.dbsetup.Operations;
 import com.ninja_squad.dbsetup.destination.DataSourceDestination;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.RuleChain;
 
 import javax.sql.DataSource;
 
@@ -32,44 +34,34 @@ import static com.kwery.models.SqlQueryExecution.COLUMN_QUERY_RUN_ID_FK;
 import static com.kwery.models.SqlQueryExecution.COLUMN_RESULT;
 import static com.kwery.models.SqlQueryExecution.COLUMN_STATUS;
 import static com.kwery.models.SqlQueryExecution.Status.SUCCESS;
+import static com.kwery.tests.fluentlenium.RepoDashFluentLeniumTest.TIMEOUT_SECONDS;
 import static com.kwery.tests.fluentlenium.utils.DbUtil.getDatasource;
 import static com.ninja_squad.dbsetup.Operations.insertInto;
-import static junit.framework.TestCase.fail;
 
-public class OnboardingShowHomeScreenUiTest extends RepoDashFluentLeniumTest {
+public class OnboardingShowHomeScreenUiTest extends ChromeFluentTest {
+    protected NinjaServerRule ninjaServerRule = new NinjaServerRule();
+
+    @Rule
+    public RuleChain ruleChain = RuleChain.outerRule(ninjaServerRule).around(new LoginRule(ninjaServerRule, this));
+
     @Before
     public void setUpOnboardingShowExecutingSqlQueriesPageTest() throws InterruptedException {
-        UserTableUtil userTableUtil = new UserTableUtil();
         DataSource datasource = getDatasource();
 
         DbSetup dbSetup = new DbSetup(new DataSourceDestination(datasource),
                 Operations.sequenceOf(
-                        userTableUtil.insertOperation(),
                         insertInto(Datasource.TABLE)
                                 .columns(COLUMN_ID, COLUMN_LABEL, COLUMN_PASSWORD, COLUMN_PORT, COLUMN_TYPE, COLUMN_URL, COLUMN_USERNAME)
                                 .values(1, "testDatasource", "password", 3306, MYSQL.name(), "foo.com", "foo").build(),
                         insertInto(SqlQuery.TABLE)
                                 .columns(SqlQuery.COLUMN_ID, COLUMN_CRON_EXPRESSION, SqlQuery.COLUMN_LABEL, COLUMN_QUERY, COLUMN_DATASOURCE_ID_FK)
-                                .values(1, "* * * * *", "testQuery", "select * from foo", 1).build(),
-                        insertInto(SqlQueryExecution.TABLE)
-                                .columns(SqlQueryExecution.COLUMN_ID, COLUMN_EXECUTION_END, COLUMN_EXECUTION_ID, COLUMN_EXECUTION_START, COLUMN_RESULT, COLUMN_STATUS, COLUMN_QUERY_RUN_ID_FK)
-                                .values(1, 1475159940797l, "thik-3456-lkdsjkfkl-lskjdfkl", 1475158740747l, "result", SUCCESS, 1) //Thu Sep 29 19:49:00 IST 2016  - Thu Sep 29 20:09:00 IST 2016
-                                .build()
+                                .values(1, "* * * * *", "testQuery", "select * from foo", 1).build()
                 )
         );
 
         dbSetup.launch();
 
-        UserLoginPage loginPage = createPage(UserLoginPage.class);
-        loginPage.withDefaultUrl(getServerAddress());
-        goTo(loginPage);
-        if (!loginPage.isRendered()) {
-            fail("Could not render login page");
-        }
-        loginPage.submitForm(userTableUtil.firstRow().getUsername(), userTableUtil.firstRow().getPassword());
-        loginPage.waitForSuccessMessage(userTableUtil.firstRow());
-
-        goTo(getServerAddress() + "/");
+        goTo(ninjaServerRule.getServerUrl() + "/");
     }
 
     @Test
--- src/test/java/com/kwery/tests/fluentlenium/onboarding/OnboardingUserAddedPage.java
+++ src/test/java/com/kwery/tests/fluentlenium/onboarding/OnboardingUserAddedPage.java
@@ -1,14 +1,16 @@
 package com.kwery.tests.fluentlenium.onboarding;
 
-import com.kwery.tests.fluentlenium.RepoDashPage;
 import com.kwery.models.User;
+import com.kwery.tests.fluentlenium.RepoDashPage;
 import org.fluentlenium.core.FluentPage;
 import org.fluentlenium.core.annotation.AjaxElement;
 import org.fluentlenium.core.domain.FluentWebElement;
 import org.openqa.selenium.support.FindBy;
 
-import static java.text.MessageFormat.format;
+import java.util.concurrent.TimeUnit;
+
 import static com.kwery.tests.util.Messages.ONBOARDING_ROOT_USER_CREATED_M;
+import static java.text.MessageFormat.format;
 
 public class OnboardingUserAddedPage extends FluentPage implements RepoDashPage {
     @AjaxElement(timeOutInSeconds = 30)
@@ -22,7 +24,8 @@ public class OnboardingUserAddedPage extends FluentPage implements RepoDashPage 
 
     @Override
     public boolean isRendered() {
-        return jumbotron.isDisplayed();
+        await().atMost(30, TimeUnit.SECONDS).until(jumbotron).isDisplayed();
+        return true;
     }
 
     public boolean containsAdminUserCreatedMessage(User user) {
