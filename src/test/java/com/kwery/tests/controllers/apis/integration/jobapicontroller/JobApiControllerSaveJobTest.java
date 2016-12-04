bdd7392bd39fb89c5a7a5625e2bfc2ee7229ffeb
abhirama <abhyrama@gmail.com>
2016-12-04 23:19:10 +0530

    JobApiController save test case.

---
+++ src/test/java/com/kwery/tests/controllers/apis/integration/jobapicontroller/JobApiControllerSaveJobTest.java
@@ -0,0 +1,72 @@
+package com.kwery.tests.controllers.apis.integration.jobapicontroller;
+
+import com.kwery.controllers.apis.JobApiController;
+import com.kwery.dtos.JobDto;
+import com.kwery.dtos.SqlQueryDto;
+import com.kwery.models.Datasource;
+import com.kwery.tests.controllers.apis.integration.userapicontroller.AbstractPostLoginApiTest;
+import com.kwery.tests.fluentlenium.utils.DbUtil;
+import com.kwery.tests.util.MysqlDockerRule;
+import com.ninja_squad.dbsetup.DbSetup;
+import com.ninja_squad.dbsetup.destination.DataSourceDestination;
+import ninja.Router;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.isJson;
+import static com.kwery.models.Datasource.*;
+import static com.kwery.models.Datasource.Type.MYSQL;
+import static com.kwery.tests.util.TestUtil.jobDtoWithoutId;
+import static com.kwery.tests.util.TestUtil.sqlQueryDtoWithoutId;
+import static com.kwery.views.ActionResult.Status.success;
+import static com.ninja_squad.dbsetup.Operations.insertInto;
+import static com.ninja_squad.dbsetup.Operations.sequenceOf;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class JobApiControllerSaveJobTest extends AbstractPostLoginApiTest {
+    @Rule
+    public MysqlDockerRule mysqlDockerRule = new MysqlDockerRule();
+
+    protected Datasource datasource;
+
+    @Before
+    public void setUpJobApiControllerSaveJobTest() {
+        datasource = mysqlDockerRule.getMySqlDocker().datasource();
+        datasource.setId(1);
+
+        new DbSetup(
+                new DataSourceDestination(DbUtil.getDatasource()),
+                sequenceOf(
+                        insertInto(Datasource.TABLE)
+                                .columns(COLUMN_ID, COLUMN_LABEL, COLUMN_PASSWORD, COLUMN_PORT, COLUMN_TYPE, COLUMN_URL, COLUMN_USERNAME)
+                                .values(datasource.getId(), datasource.getLabel(), datasource.getPassword(), datasource.getPort(), MYSQL.name(), datasource.getUrl(), datasource.getUsername())
+                                .build()
+                )
+        ).launch();
+    }
+
+    @Test
+    public void test() {
+        String url = getInjector().getInstance(Router.class).getReverseRoute(JobApiController.class, "saveJob");
+
+        JobDto jobDto = jobDtoWithoutId();
+        jobDto.setCronExpression("* * * * *");
+        jobDto.setSqlQueries(new ArrayList<>(1));
+
+        SqlQueryDto sqlQueryDto = sqlQueryDtoWithoutId();
+        sqlQueryDto.setQuery("select * from mysql.user");
+        sqlQueryDto.setDatasourceId(datasource.getId());
+
+        jobDto.getSqlQueries().add(sqlQueryDto);
+
+        String response = ninjaTestBrowser.postJson(getUrl(url), jobDto);
+
+        assertThat(response, isJson());
+        assertThat(response, hasJsonPath("$.status", is(success.name())));
+    }
+}
