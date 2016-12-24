3e6d49c27ade94318b77a4ec1a3c4df2b19ec09a
abhirama <abhyrama@gmail.com>
2016-12-24 23:54:10 +0530

    Scheduling jobs on startup.

--- src/main/java/com/kwery/services/job/JobService.java
+++ src/main/java/com/kwery/services/job/JobService.java
@@ -1,14 +1,17 @@
 package com.kwery.services.job;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Strings;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.kwery.dao.JobDao;
 import com.kwery.models.JobModel;
 import it.sauronsoftware.cron4j.TaskExecutor;
+import ninja.lifecycle.Start;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -37,6 +40,17 @@ public class JobService {
         String schedulerId = kweryScheduler.schedule(jobModel.getCronExpression(), jobTask);
         jobIdSchedulerIdMap.put(jobId, schedulerId);
         return schedulerId;
+    }
+
+    @Start
+    public void scheduleAllJobs() {
+        List<JobModel> jobModels = jobDao.getAllJobs();
+        for (JobModel jobModel : jobModels) {
+            if (!"".equals(Strings.nullToEmpty(jobModel.getCronExpression()))) {
+                logger.info("Scheduling job with id {}", jobModel.getId());
+                schedule(jobModel.getId());
+            }
+        }
     }
 
     public void launch(int jobId) {
