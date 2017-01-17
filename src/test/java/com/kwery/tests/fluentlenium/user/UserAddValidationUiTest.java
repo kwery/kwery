10eefebd68f7ac1d171be32dd3de6aac844d0eaf
abhirama <abhyrama@gmail.com>
2017-01-17 10:04:49 +0530

    Removed dependency of user add page on JQuery UI and validation.

--- src/test/java/com/kwery/tests/fluentlenium/user/UserAddValidationUiTest.java
+++ src/test/java/com/kwery/tests/fluentlenium/user/UserAddValidationUiTest.java
@@ -2,8 +2,7 @@ package com.kwery.tests.fluentlenium.user;
 
 import org.junit.Test;
 
-import static com.kwery.tests.util.Messages.PASSWORD_VALIDATION_M;
-import static com.kwery.tests.util.Messages.USERNAME_VALIDATION_M;
+import static com.kwery.tests.fluentlenium.user.UserAddPage.INPUT_VALIDATION_ERROR_MESSAGE;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -11,7 +10,7 @@ public class UserAddValidationUiTest extends UserAddUiTest {
     @Test
     public void test() {
         page.submitForm();
-        assertThat(page.usernameValidationErrorMessage(), is(USERNAME_VALIDATION_M));
-        assertThat(page.passwordValidationErrorMessage(), is(PASSWORD_VALIDATION_M));
+        assertThat(page.usernameValidationErrorMessage(), is(INPUT_VALIDATION_ERROR_MESSAGE));
+        assertThat(page.passwordValidationErrorMessage(), is(INPUT_VALIDATION_ERROR_MESSAGE));
     }
 }
