aea84f9f98a17c70bc7c6e7420d7f7e6a4bc21f2
abhirama <abhyrama@gmail.com>
2017-01-27 11:28:38 +0530

    Added job search filter functionality.

---
+++ src/main/java/com/kwery/services/job/JobSearchFilter.java
@@ -0,0 +1,33 @@
+package com.kwery.services.job;
+
+import java.util.Set;
+
+public class JobSearchFilter {
+    protected int resultCount;
+    protected int pageNo;
+    protected Set<Integer> jobLabelIds;
+
+    public int getResultCount() {
+        return resultCount;
+    }
+
+    public void setResultCount(int resultCount) {
+        this.resultCount = resultCount;
+    }
+
+    public int getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(int pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public Set<Integer> getJobLabelIds() {
+        return jobLabelIds;
+    }
+
+    public void setJobLabelIds(Set<Integer> jobLabelIds) {
+        this.jobLabelIds = jobLabelIds;
+    }
+}
