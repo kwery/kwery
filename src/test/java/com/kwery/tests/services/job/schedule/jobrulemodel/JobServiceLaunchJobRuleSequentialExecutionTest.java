ae1d04e45a2a6e1c111889068bc2c31f246db5b8
abhirama <abhyrama@gmail.com>
2017-02-09 18:21:15 +0530

    Incorporated sequential job execution in JobTask.

---
+++ src/test/java/com/kwery/tests/services/job/schedule/jobrulemodel/JobServiceLaunchJobRuleSequentialExecutionTest.java
@@ -0,0 +1,40 @@
+package com.kwery.tests.services.job.schedule.jobrulemodel;
+
+import com.kwery.models.JobRuleModel;
+import com.kwery.tests.services.job.launch.jobrulemodel.JobServiceLaunchJobJobRuleModelAbstractTest;
+import com.kwery.tests.util.TestUtil;
+import org.junit.Test;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+public class JobServiceLaunchJobRuleSequentialExecutionTest extends JobServiceLaunchJobJobRuleModelAbstractTest {
+    @Override
+    public String getQuery0() {
+        return "select User from mysql.user where User = 'root'";
+    }
+
+    @Override
+    public String getQuery1() {
+        return "select User from mysql.user where User = 'root'";
+    }
+
+    @Override
+    public String getQuery2() {
+        return "select User from mysql.user where User = 'root'";
+    }
+
+    @Override
+    public JobRuleModel getJobRuleMode() {
+        JobRuleModel jobRuleModel = TestUtil.jobRuleModel();
+        jobRuleModel.setSequentialSqlQueryExecution(true);
+        jobRuleModel.setStopExecutionOnSqlQueryFailure(false);
+        return jobRuleModel;
+    }
+
+    @Test
+    public void test() {
+        jobService.schedule(jobModel.getId());
+        waitUntilExecutions(2, MINUTES, 1);
+        assertQueriesWereExecutedSequentially(3);
+    }
+}
---
+++ src/test/java/com/kwery/tests/services/job/schedule/jobrulemodel/JobServiceLaunchJobRuleSequentialExecutionWithFailureTest.java
@@ -0,0 +1,40 @@
+package com.kwery.tests.services.job.schedule.jobrulemodel;
+
+import com.kwery.models.JobRuleModel;
+import com.kwery.tests.services.job.launch.jobrulemodel.JobServiceLaunchJobJobRuleModelAbstractTest;
+import org.junit.Test;
+
+import static com.kwery.tests.util.TestUtil.jobRuleModel;
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+public class JobServiceLaunchJobRuleSequentialExecutionWithFailureTest extends JobServiceLaunchJobJobRuleModelAbstractTest {
+    @Override
+    public String getQuery0() {
+        return "select * from foo";
+    }
+
+    @Override
+    public String getQuery1() {
+        return "select User from mysql.user where User = 'root'";
+    }
+
+    @Override
+    public String getQuery2() {
+        return "select User from mysql.user where User = 'root'";
+    }
+
+    @Override
+    public JobRuleModel getJobRuleMode() {
+        JobRuleModel jobRuleModel = jobRuleModel();
+        jobRuleModel.setSequentialSqlQueryExecution(true);
+        jobRuleModel.setStopExecutionOnSqlQueryFailure(false);
+        return jobRuleModel;
+    }
+
+    @Test
+    public void test() {
+        jobService.schedule(jobModel.getId());
+        waitUntilExecutions(2, MINUTES, 1);
+        assertQueriesWereExecutedSequentially(3);
+    }
+}
