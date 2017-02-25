bc17462959eba3cb550c08da501a6fec741702c4
abhirama <abhyrama@gmail.com>
2017-02-25 14:50:02 +0530

    Added screenshot ability.

--- src/test/java/com/kwery/tests/util/ChromeFluentTest.java
+++ src/test/java/com/kwery/tests/util/ChromeFluentTest.java
@@ -5,8 +5,18 @@ import org.fluentlenium.adapter.junit.FluentTest;
 import java.util.concurrent.TimeUnit;
 
 public class ChromeFluentTest extends FluentTest {
-@Override
+    @Override
     public Long getImplicitlyWait() {
         return TimeUnit.MINUTES.toMillis(1);
+    }
+
+    @Override
+    public TriggerMode getScreenshotMode() {
+        return TriggerMode.AUTOMATIC_ON_FAIL;
+    }
+
+    @Override
+    public String getScreenshotPath() {
+        return "/tmp";
     }
 }
