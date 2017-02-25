8fe4f233f3a0529e25438d0fdd44eae57e4059f7
abhirama <abhyrama@gmail.com>
2017-02-25 17:52:33 +0530

    Added a global timeout for selenium test cases.

--- src/test/java/com/kwery/tests/util/ChromeFluentTest.java
+++ src/test/java/com/kwery/tests/util/ChromeFluentTest.java
@@ -1,10 +1,15 @@
 package com.kwery.tests.util;
 
 import org.fluentlenium.adapter.junit.FluentTest;
+import org.junit.Rule;
+import org.junit.rules.Timeout;
 
 import java.util.concurrent.TimeUnit;
 
 public class ChromeFluentTest extends FluentTest {
+    @Rule
+    public Timeout globalTimeout = new Timeout(new Long(TimeUnit.MINUTES.toMillis(3)).intValue());
+
     @Override
     public Long getImplicitlyWait() {
         return TimeUnit.MINUTES.toMillis(1);
