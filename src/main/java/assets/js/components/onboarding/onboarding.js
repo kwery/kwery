fb34f43563b9b08f87b01ba3e3b4bf9ceb67ada1
abhirama <abhyrama@gmail.com>
2017-03-10 00:26:17 +0530

    Problem:  An onboarding page with steps was unnecessarily shown if a user signed up, logged in, logged out and then logged in again or signed up, logged in and tried to go to /.
    Solution: Nuked this page and forcing the user to complete the necessary step of onboarding.

--- src/main/java/assets/js/components/onboarding/onboarding.js
+++ src/main/java/assets/js/components/onboarding/onboarding.js
@@ -2,12 +2,6 @@ define(["knockout", "jquery", "text!components/onboarding/onboarding.html", "aja
     function viewModel(params) {
         var self = this;
 
-        self.showNextSteps = ko.observable(false);
-        self.showAddDatasource = ko.observable(false);
-        self.showAddJob = ko.observable(false);
-
-        self.message = ko.observable(ko.i18n("onboarding.default.message"));
-
         ajaxUtil.waitingAjax({
             url: "/api/onboarding/next-action",
             type: "GET",
@@ -15,21 +9,16 @@ define(["knockout", "jquery", "text!components/onboarding/onboarding.html", "aja
             success: function(response){
                 switch (response.action) {
                     case "LOGIN":
-                        self.showNextSteps(false);
                         window.location.href = "/#user/login";
                         break;
                     case "SIGN_UP":
-                        self.showNextSteps(false);
                         window.location.href = "/#user/sign-up?onboarding=true";
                         break;
                     case "ADD_DATASOURCE":
-                        self.showNextSteps(true);
-                        self.showAddJob(true);
-                        self.showAddDatasource(true);
+                        window.location.href = "/#datasource/add?onboarding=true";
                         break;
                     case "ADD_JOB":
-                        self.showNextSteps(true);
-                        self.showAddJob(true);
+                        window.location.href = "/#report/add?onboarding=true";
                         break;
                     case "SHOW_HOME_SCREEN":
                         window.location.href = "/#report/list";
