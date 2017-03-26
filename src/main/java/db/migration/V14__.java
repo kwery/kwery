e0ed991466b01512536bafc93779c6e1a3d6a5bc
abhirama <abhyrama@gmail.com>
2017-03-26 14:33:07 +0530

    Problem: Kwery was shutting down saying license period is over.
    Solution: Installation date was inadvertently set to current time minus 28 days for testing which was committed reverted that.

--- src/main/java/db/migration/V14__.java
+++ src/main/java/db/migration/V14__.java
@@ -4,13 +4,12 @@ import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.util.concurrent.TimeUnit;
 
 public class V14__ implements JdbcMigration {
     @Override
     public void migrate(Connection connection) throws Exception {
         try (PreparedStatement ps = connection.prepareStatement("insert into installation_detail (installation_epoch) values (?)")) {
-            ps.setLong(1, System.currentTimeMillis() - TimeUnit.DAYS.toMillis(28));
+            ps.setLong(1, System.currentTimeMillis());
             ps.executeUpdate();
         }
     }
