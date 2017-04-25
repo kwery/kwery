b36b0d93dadda2c840212ac95229ddf475abe8d9
abhirama <abhyrama@gmail.com>
2017-04-25 11:18:33 +0530

    Added logo_url column length validation for report email configuration model.

--- src/main/java/com/kwery/models/ReportEmailConfigurationModel.java
+++ src/main/java/com/kwery/models/ReportEmailConfigurationModel.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = ReportEmailConfigurationModel.REPORT_EMAIL_CONFIGURATION_TABLE)
@@ -11,8 +12,12 @@ public class ReportEmailConfigurationModel extends AbstractBaseModel {
     public static final String REPORT_EMAIL_CONFIGURATION_TABLE = "report_email_configuration";
     public static final String LOGO_URL_COLUMN = "logo_url";
 
+    public static final int LOGO_URL_MAX = 2083;
+    public static final int LOGO_URL_MIN = 1;
+
     @NotNull
     @Column(name = LOGO_URL_COLUMN)
+    @Size(min = LOGO_URL_MIN, max = LOGO_URL_MAX)
     protected String logoUrl;
 
     public String getLogoUrl() {
